31c020bd7215d921a7543519ffe94656215cf901
Michael Zinn <MichaelGZinn@gmail.com>
2022-05-13 15:41:52 +0200

    Add support for multiple transactions with the same amount on the same day

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ License is LGPL v3
 
 Upload a single CSV file that is already in a YNAB compatible format to a single account in one of your budgets.
 
+If this crashes on Apple Silicon, make sure to use at least Ruby 2.7.5.
+
 ### How to use
 
 ruby ynab_csv_upload.rb APITOKEN BUDGETID ACCOUNTID CSVPATH
@@ -15,5 +17,3 @@ ruby ynab_csv_upload.rb APITOKEN BUDGETID ACCOUNTID CSVPATH
 ### Differences to uploading via the web app
 
 * Future transactions will be ignored. (The web app changes their date to today)
-* Having two identical transactions on the same day is currently undefined behavior. This can be fixed though, the problem is the ":1" at the end of IDs, which should count up for identical transactions. Feel free to fix it.
-
--- ynab_csv_upload.rb
+++ ynab_csv_upload.rb
@@ -1,6 +1,8 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 # Imports a YNAB CSV into a YNAB budget's account
+
+puts "If this crashes on Apple Silicon, make sure to use at least Ruby 2.7.5!"
 
 require 'bigdecimal'
 require 'csv'
@@ -12,32 +14,58 @@ access_token, budget_id, account_id, csv_path = ARGV
 
 ynab = YNAB::API.new(access_token)
 
-def generate_import_id_for(transaction) 
-  "YNAB:#{transaction[:amount]}:#{transaction[:date].to_s}:1"
+# Transactions get identified by date+amount, so if you have multiple with
+# The same date+amount, unique_count must differ. It is recommended to
+# start unique_count at 1 and increment for each additional transaction with
+# the same date+amount
+def generate_import_id_for(transaction, unique_count)
+  "YNAB:#{transaction[:amount]}:#{transaction[:date].to_s}:#{unique_count}"
+end
+
+# Pass a complete csv as string and get the transactions back.
+# Assumes that the CSV contains a header row.
+def csv_to_transactions(account_id, ynab_csv)
+  rows = ynab_csv.drop(1)   # drop the column names
+
+  # fill with imperative code to hand duplicate ids more easily
+  previous_transactions = []
+
+  rows.each do |row|
+    usa_date, payee, memo, stringamount = row
+    new_transaction = {
+      account_id: account_id,
+      amount: ((BigDecimal(stringamount) * 1000).to_i), # milliunits format
+      cleared: "Cleared",
+      date: Date.strptime(usa_date, "%m/%d/%y"), # random USA order: 12/31/93
+      memo: memo[0,200],
+      payee_name: payee
+    }
+    collision_count = previous_transactions.count do |previous_transaction|
+      new_transaction[:amount] == previous_transaction[:amount] &&
+      new_transaction[:date] == previous_transaction[:date]
+    end
+    new_transaction[:import_id] = generate_import_id_for(
+      new_transaction,
+      collision_count + 1
+    )
+    previous_transactions << new_transaction
+  end
+  previous_transactions
 end
 
 # Load transactions from given CSV file
 ynab_csv     = CSV.read(csv_path)
-rows         = ynab_csv.drop(1)   # drop the column names
-transactions = rows.map do |row|
-  usa_date, payee, memo, stringamount = row
-  transaction = {
-    account_id: account_id,
-    amount: ((BigDecimal(stringamount) * 1000).to_i), # milliunits format
-    cleared: "Cleared",
-    date: Date.strptime(usa_date, "%m/%d/%y"), # random USA order: 12/31/93
-    memo: memo[0,200],
-    payee_name: payee
-  }
-  transaction[:import_id] = generate_import_id_for(transaction)
-  transaction
-end.select do |transaction|
-  transaction[:date] <= Date.today
+all_transactions = csv_to_transactions(account_id, ynab_csv)
+transactions_that_can_be_uploaded = all_transactions.select do |transaction|
+  transaction[:date] <= Date.today # Future transactions don't work.
 end
 
 # pp transactions
 begin
-  pp ynab.transactions.create_transaction( budget_id, {transactions: transactions})
+  pp ynab.transactions.create_transaction(
+    budget_id,
+    { transactions: transactions_that_can_be_uploaded }
+  )
 rescue YNAB::ApiError => e
   puts "ERROR: id=#{e.id}; name=#{e.name}; detail: #{e.detail}"
 end
